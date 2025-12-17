cb43abe1dd358cb5a677f49e932b4058842429d0
Amogh Panhale <amoghmpanhale@gmail.com>
2025-12-17 13:41:17 -0700

    fix: Got the hamburger menu working!

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 /* Open */
 function openNav() {
     document.getElementById("navOverlay").style.width = "250px";
+    document.querySelector(".hamburger").style.display = "none";
 }
   
-  /* Close */
-  function closeNav() {
-    document.getElementById("navOverlay").style.width = "0%";
-} 
+/* Close */
+function closeNav() {
+    document.getElementById("navOverlay").style.width = "0";
+    document.querySelector(".hamburger").style.display = "block";
+}
