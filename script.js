91b2c65e5356cf9d4db85ac1640ab1023880183c
amoghpanhale <amoghmpanhale@gmail.com>
2025-12-18 20:14:30 -0700

    feat: added dropdown indexing to the main research page

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ function closeNav() {
     document.getElementById("navOverlay").style.width = "0";
     document.querySelector(".hamburger").style.display = "block";
 }
+
+/* Toggling Topics on the main research page */
+function toggleTopic(header) {
+    var dropdown = header.nextElementSibling;
+    var arrow = header.querySelector('.arrow');
+    
+    if (dropdown.style.maxHeight) {
+        dropdown.style.maxHeight = null;
+        arrow.textContent = '▶';
+    } else {
+        dropdown.style.maxHeight = dropdown.scrollHeight + 'px';
+        arrow.textContent = '▼';
+    }
+}
